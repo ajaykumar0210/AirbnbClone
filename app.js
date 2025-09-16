c9883d62ded075212bb0b4409c3f7b923c4d3dab
ajaykumar0210 <ajaykumar021043@gmail.com>
2025-09-16 23:54:41 +0530

    changes

--- app.js
+++ app.js
@@ -1,99 +1,139 @@
-const express = require("express");
-const app = express();
-const mongoose = require("mongoose");
-const Listing = require("./models/listing");
-const path = require("path");
-const methodOverride = require("method-override");
-const ejsMate = require("ejs-mate");
+if(process.env.NODE_ENV != "production") {
+    require("dotenv").config();
+}
+// console.log(process.env);
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+const express=require("express");
+const app=express();
+const mongoose=require("mongoose");
+const path= require("path");
+const methodOverride= require("method-override");
+const ejsMate= require("ejs-mate");
+const ExpressError= require("./utils/ExpressError.js");
+const listingRouter= require("./routes/listing.js");
+const reviewRouter= require("./routes/review.js");
+const userRouter= require("./routes/user.js");
+
+const session= require("express-session");
+const MongoStore = require('connect-mongo'); // for session store on cloud database not on local machine
+
+
+const flash=require("connect-flash");
+const passport= require("passport");
+const LocalStrategy= require("passport-local");
+const User= require("./models/user.js");
+
+//Database connection
+// const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
+
+const dbUrl = 'mongodb://localhost:27017/wanderlust'; // TO CONNECT TO MONGO ATLAS DATABASE ON CLOUD
+
+
 
 main()
-.then(() => {
-    console.log("connected to DB");
-})
-.catch((err)=> {
-    console.log(err);
-});
+    .then(()=>console.log("connected to db"))
+    .catch(err=>console.log(err));
 
-async function main() {
-    await mongoose.connect(MONGO_URL);
+async function main(){
+    await mongoose.connect(dbUrl);
 }
 
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded({extended: true}));
+//set ejs as view engine
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"/views"));
+app.use(express.json()); // for taking body from hoppscotch
+app.use(express.urlencoded({ extended: true})); // to take form data into req.body in urlencoded form
 app.use(methodOverride("_method"));
-app.engine('ejs', ejsMate);
-app.use(express.static(path.join(__dirname, "/public")));
+app.engine("ejs",ejsMate);
+app.use(express.static(path.join(__dirname,"/public")));
+//This async error can be handled by default compiler
+//except database errors and ejs errors
 
-app.get("/", (req, res) => {
-    res.send("Hi, I am Ajay Kumar");
+// app.use(async ()=>{
+//     throw await Error("user defined error");
+// })
+
+const store = MongoStore.create({  // for session store on cloud database not on local machine
+    mongoUrl: dbUrl,
+    crypto: {
+        secret: 'secret',
+    },
+    touchAfter: 24*3600, 
+
 });
 
-//Index Route
-app.get("/listings", async (req,res) =>{
-    const allListings = await Listing.find({});
-    res.render("listings/index.ejs", {allListings});
-});
+const sessionOptions={
+    store,
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: true,
+    cookie:{
+        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+        maxAge:  7 * 24 * 60 * 60 * 1000,
+        httpOnly: true
+    }
+};
 
-// New Route
-app.get("/listings/new", (req,res) => {
-    res.render("listings/new.ejs");
+
+app.use(session(sessionOptions));
+app.use(flash());
+
+
+app.use(passport.initialize());//middleware that initialize the passport
+app.use(passport.session()); //to store the current user details in the session
+passport.use(new LocalStrategy(User.authenticate())); // verifies(user,password) with the database users
+
+passport.serializeUser(User.serializeUser()); //stores userdetails in cookies for a session
+passport.deserializeUser(User.deserializeUser()); //removes the userdetails when session is over
+
+
+//res.locals have scope of current request only
+//changes to locals of one request does not affect to locals of others.
+app.use((req,res,next)=>{
+    res.locals.error= req.flash("error");
+    res.locals.success= req.flash("success");
+    res.locals.failure= req.flash("failure");
+    res.locals.currUser= req.user;
+    next();
 })
 
-//Show Route
-app.get("/listings/:id",async (req,res)=>{
-    let {id} = req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/show.ejs", {listing});
-})
-
-//Create Route
-app.post("/listings", async (req,res) => {
-    // let {title, description, image, price, coutry, locaion} = req.body;
-    const newListing = new Listing(req.body.listing);
-    await newListing.save();
-    res.redirect("/listings");
-});
-
-//Edit Route
-app.get("/listings/:id/edit",async(req,res) => {
-    let {id} = req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/edit.ejs",{listing});
-})
-
-//Update Route
-app.put("/listings/:id", async (req,res) => {
-    let {id} = req.params;
-    await Listing.findByIdAndUpdate(id,{...req.body.listing});
-    res.redirect(`/listings/${id}`);
-})
-
-//Delete Route
-app.delete("/listings/:id", async(req,res) =>{
-    let { id } = req.params;
-    let deleteListing = await Listing.findByIdAndDelete(id);
-    console.log(deleteListing);
-    res.redirect("/listings");
-})
-
-
-// app.get("/testListing", async(req, res) => {
-//     let sampleListing = new Listing({
-//         title: "My New Villa",
-//         description: "By the beach",
-//         price: 1200,
-//         location: "California, Goa",
-//         country: "India",
-//     });
-
-//     await sampleListing.save();
-//     console.log("sample was saved");
-//     res.send("Successful testing");
+//root
+// app.get("/",(req,res)=>{
+//     res.send("root page");
 // });
 
-app.listen(8000, () => {
-    console.log("Server is running on port 8000");
+//listings
+app.use("/listings",listingRouter);
+//reviews
+app.use("/listings/:id/reviews",reviewRouter);
+app.use("/",userRouter);
+
+//demo User
+
+// app.get("/demouser",async(req,res)=>{
+//     let fakeUser = new User({
+//         email: "student@gmail.com",
+//         username: "delta-student"
+//     });
+
+//     let registeredUser= await User.register(fakeUser,"helloworld");// to save the user with password
+//     res.send(registeredUser);
+// })
+
+
+//Error handling
+app.use((req,res,next)=>{
+    next(new ExpressError(404, "Page not found!"));
+})
+app.use((err,req,res,next)=>{
+    let {statusCode=500,message="Something went wrong!!!"}= err;
+    res.status(400).render("error.ejs", { message });
+
+    // res.status(statusCode).send(message); 
+})
+
+//Starting server
+app.listen(8080,()=>{
+    console.log("server is listening on port 8080");
 });
